4c88d1e6801638b021a9c7b0557322bdbc831891
Hoangnguyenduc10-06-2006 <ducgay233@gmail.com>
2026-05-26 05:13:34 +0700

    Update InventoryService.java

--- src/main/java/com/re/session9/service/InventoryService.java
+++ src/main/java/com/re/session9/service/InventoryService.java
@@ -1,18 +1,16 @@
 package com.re.session9.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class InventoryService {
-    private static final Logger logger = LoggerFactory.getLogger(InventoryService.class);
 
     public void updateStock(String productId, int qty) {
 
-        logger.info("Bắt đầu cập nhật kho cho SP: {}, SL: {}", productId, qty);
+        log.info("Bắt đầu cập nhật kho cho SP: {}, SL: {}", productId, qty);
 
     }
 }
